ac1615cb19da76e3599983667226d17418dfc19e
pdwyer2 <46718941+pdwyer2@users.noreply.github.com>
2021-11-01 17:36:48 -0500

    Added main files

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
+using System.Configuration;
+
+
+namespace dataVisualizerWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        OracleConnection con = null;
+        public MainWindow()
+        {
+            this.setConnection();
+            InitializeComponent();
+        }
+        private void updateDataGrid()
+        {
+            OracleCommand cmd = con.CreateCommand();
+
+            /*
+            cmd.CommandText =
+                "select OWNER, CONSTRAINT_NAME, CONSTRAINT_TYPE, TABLE_NAME, R_OWNER, R_CONSTRAINT_NAME " +
+                "from dba_constraints where constraint_type in('P', 'U', 'R') " +
+                "and owner = 'OT' and TABLE_NAME = 'COUNTRIES'";
+            */
+
+            cmd.CommandText =
+                 cmd.CommandText =
+                "select OWNER, CONSTRAINT_NAME, CONSTRAINT_TYPE, TABLE_NAME, R_OWNER, R_CONSTRAINT_NAME " +
+                "from dba_constraints where constraint_type in('P', 'U', 'R') " +
+                "and owner = 'OT' and TABLE_NAME = 'LOCATIONS'";
+
+            cmd.CommandType = CommandType.Text;
+            OracleDataReader dr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+            myDataGrid.ItemsSource = dt.DefaultView;
+            dr.Close();
+        }
+
+        private void checkTableConnections()
+        {
+
+        }
+        private void setConnection()
+        {
+            
+            String connectionString = ConfigurationManager.ConnectionStrings["sampleDB"].ConnectionString;
+            con = new OracleConnection(connectionString);
+            try
+            {
+                con.Open();
+            }
+            catch(Exception exp) { }
+     
+        }
+
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.updateDataGrid();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            con.Close();
+        }
+
+    }
+}
